5b91ec2e60895bcb1c3a168619ecd158d0ada9c3
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-01 14:17:00 -0500

    Bump Design Lab to 0.4.1

--- mockups/design-lab/config.js
+++ mockups/design-lab/config.js
@@ -1,5 +1,5 @@
 export const DESIGN_LAB = Object.freeze({
-  version: '0.4.0',
+  version: '0.4.1',
   buildDate: '2026-08-01',
   branch: 'feature/design-lab',
   storageKey: 'nudge-design-lab-review-v1'

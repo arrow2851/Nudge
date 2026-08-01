3e6eac273a8ff25cb04e02e9a6df4ccedee79491
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-01 17:23:30 -0500

    Update capture metadata to 0.7.1

--- mockups/design-lab/quality.js
+++ mockups/design-lab/quality.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = '0.7.0';
+  const VERSION = '0.7.1';
   const initialParams = new URLSearchParams(location.search);
   const captureMode = initialParams.get('capture');
 

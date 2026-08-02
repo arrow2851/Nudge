2e2b81a9f834c884489a4ab172d8e2b0030f3269
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-01 19:17:37 -0500

    Advance Look #1 reference labels to 0.8.1

--- mockups/design-lab/look1-reference.js
+++ mockups/design-lab/look1-reference.js
@@ -1,6 +1,6 @@
 import { getScenario } from './fixtures.js';
 
-const VERSION = '0.8.0';
+const VERSION = '0.8.1';
 const params = new URLSearchParams(location.search);
 const allowedScreens = new Set(['areas', 'area', 'intervention']);
 const screenId = allowedScreens.has(params.get('screen')) ? params.get('screen') : 'areas';

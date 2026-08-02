fa7b53a12f9d52123a6f8960750d4e7c4ab63771
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-01 19:06:03 -0500

    Advance Design Lab capture metadata to 0.8.0

--- mockups/design-lab/quality.js
+++ mockups/design-lab/quality.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = '0.7.2';
+  const VERSION = '0.8.0';
   const initialParams = new URLSearchParams(location.search);
   const captureMode = initialParams.get('capture');
 

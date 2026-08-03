38235ac3b09eed54d952fac371b8b5fb6eeb546a
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-03 13:19:30 -0500

    Wire Phase 5 recurring care into the app shell

--- app/src/main/java/com/arrow2851/nudge/MainActivity.kt
+++ app/src/main/java/com/arrow2851/nudge/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.arrow2851.nudge.ui.NudgePhase4App
+import com.arrow2851.nudge.ui.NudgePhase5App
 import com.arrow2851.nudge.ui.theme.NudgeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NudgeTheme {
-                NudgePhase4App()
+                NudgePhase5App()
             }
         }
     }

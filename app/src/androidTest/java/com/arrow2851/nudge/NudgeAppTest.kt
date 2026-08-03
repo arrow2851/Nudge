0465385fdb37e71b72d5d85c5b2ab830efd71c08
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-03 11:57:55 -0500

    Fix Compose test API import

--- app/src/androidTest/java/com/arrow2851/nudge/NudgeAppTest.kt
+++ app/src/androidTest/java/com/arrow2851/nudge/NudgeAppTest.kt
@@ -3,7 +3,6 @@ package com.arrow2851.nudge
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText

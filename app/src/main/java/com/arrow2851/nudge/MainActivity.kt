8f07955ff3d20ee46c1af2bde03f84e2dc3c6d79
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-03 11:35:21 -0500

    Launch the Phase 4 application shell

--- app/src/main/java/com/arrow2851/nudge/MainActivity.kt
+++ app/src/main/java/com/arrow2851/nudge/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.arrow2851.nudge.ui.NudgeApp
+import com.arrow2851.nudge.ui.NudgePhase4App
 import com.arrow2851.nudge.ui.theme.NudgeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NudgeTheme {
-                NudgeApp()
+                NudgePhase4App()
             }
         }
     }

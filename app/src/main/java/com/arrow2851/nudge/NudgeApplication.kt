b07f688d45a11940aaf98a089c041c9bd85fccb0
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-03 09:38:51 -0500

    Configure Hilt WorkManager factory

--- app/src/main/java/com/arrow2851/nudge/NudgeApplication.kt
+++ app/src/main/java/com/arrow2851/nudge/NudgeApplication.kt
@@ -1,7 +1,18 @@
 package com.arrow2851.nudge
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class NudgeApplication : Application()
+class NudgeApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}

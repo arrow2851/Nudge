f1142c2cbba60e620cddcd3116e9db47f38e926c
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-03 00:08:18 -0500

    Add Compose launch activity

---
+++ app/src/main/java/com/arrow2851/nudge/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.arrow2851.nudge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.arrow2851.nudge.ui.NudgeApp
+import com.arrow2851.nudge.ui.theme.NudgeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NudgeTheme {
+                NudgeApp()
+            }
+        }
+    }
+}

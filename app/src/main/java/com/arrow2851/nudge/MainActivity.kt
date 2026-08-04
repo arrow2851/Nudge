e5554f6739f2133e8ea411066f956350957a95bd
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-03 23:47:00 -0500

    Handle widget and shortcut entry points

--- app/src/main/java/com/arrow2851/nudge/MainActivity.kt
+++ app/src/main/java/com/arrow2851/nudge/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.arrow2851.nudge.ui.InterventionSettingsRoute
 import com.arrow2851.nudge.ui.NudgePhase7App
+import com.arrow2851.nudge.ui.components.NudgeDestination
 import com.arrow2851.nudge.ui.theme.NudgeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,10 +15,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val destination = intent?.data?.host
+        val initialRoute = when (destination) {
+            "interventions" -> InterventionSettingsRoute
+            else -> NudgeDestination.Today.route
+        }
+        val openQuickAdd = destination == "quick-add" ||
+            intent?.getBooleanExtra(ExtraOpenQuickAdd, false) == true
         setContent {
             NudgeTheme {
-                NudgePhase7App()
+                NudgePhase7App(
+                    initialRoute = initialRoute,
+                    openQuickAddInitially = openQuickAdd,
+                )
             }
         }
+    }
+
+    companion object {
+        const val ExtraOpenQuickAdd = "open_quick_add"
     }
 }

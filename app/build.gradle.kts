50bba4c1c6bb44b1106922ea2b78a980313099e1
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-04 00:02:58 -0500

    Advance development version for intervention and quick access

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.arrow2851.nudge"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.8.0-dev"
+        versionCode = 2
+        versionName = "0.10.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true

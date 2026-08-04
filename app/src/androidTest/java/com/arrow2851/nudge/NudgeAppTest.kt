0217c67f67f4cf4fde3b1349de2b6124df76c8a6
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-04 00:32:39 -0500

    Fix Quick Add instrumentation test import

--- app/src/androidTest/java/com/arrow2851/nudge/NudgeAppTest.kt
+++ app/src/androidTest/java/com/arrow2851/nudge/NudgeAppTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText

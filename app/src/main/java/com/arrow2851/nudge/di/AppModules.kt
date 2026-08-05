80a2428f2b29bf167149e55983b7f4f758c44734
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-04 23:47:27 -0500

    Bind history repository and DAO

--- app/src/main/java/com/arrow2851/nudge/di/AppModules.kt
+++ app/src/main/java/com/arrow2851/nudge/di/AppModules.kt
@@ -10,10 +10,12 @@ import com.arrow2851.nudge.core.data.AreaRepository
 import com.arrow2851.nudge.core.data.ChoreRepository
 import com.arrow2851.nudge.core.data.CompletionRepository
 import com.arrow2851.nudge.core.data.DataStorePreferencesRepository
+import com.arrow2851.nudge.core.data.HistoryRepository
 import com.arrow2851.nudge.core.data.ListWorkflowRepository
 import com.arrow2851.nudge.core.data.LocalAreaRepository
 import com.arrow2851.nudge.core.data.LocalChoreRepository
 import com.arrow2851.nudge.core.data.LocalCompletionRepository
+import com.arrow2851.nudge.core.data.LocalHistoryRepository
 import com.arrow2851.nudge.core.data.LocalReusableListRepository
 import com.arrow2851.nudge.core.data.LocalTaskRepository
 import com.arrow2851.nudge.core.data.PreferencesRepository
@@ -25,6 +27,7 @@ import com.arrow2851.nudge.core.data.TaskRepository
 import com.arrow2851.nudge.core.database.AreaDao
 import com.arrow2851.nudge.core.database.ChoreDao
 import com.arrow2851.nudge.core.database.CompletionDao
+import com.arrow2851.nudge.core.database.HistoryDao
 import com.arrow2851.nudge.core.database.NudgeDatabase
 import com.arrow2851.nudge.core.database.NudgeMigrations
 import com.arrow2851.nudge.core.database.ReusableListDao
@@ -83,6 +86,9 @@ object PersistenceModule {
     fun provideCompletionDao(database: NudgeDatabase): CompletionDao = database.completionDao()
 
     @Provides
+    fun provideHistoryDao(database: NudgeDatabase): HistoryDao = database.historyDao()
+
+    @Provides
     fun provideReusableListDao(database: NudgeDatabase): ReusableListDao = database.reusableListDao()
 
     @Provides
@@ -118,6 +124,12 @@ abstract class RepositoryModule {
     abstract fun bindCompletionRepository(
         implementation: LocalCompletionRepository,
     ): CompletionRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindHistoryRepository(
+        implementation: LocalHistoryRepository,
+    ): HistoryRepository
 
     @Binds
     @Singleton

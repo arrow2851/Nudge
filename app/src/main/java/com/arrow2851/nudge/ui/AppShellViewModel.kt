da705ae1d8c4b011dbf1584ef6fcbdbafe9849fd
arrow2851 <omar.h.mohiuddin@gmail.com>
2026-08-04 23:53:58 -0500

    Expose centralized app feedback and undo to Compose

---
+++ app/src/main/java/com/arrow2851/nudge/ui/AppShellViewModel.kt
@@ -0,0 +1,26 @@
+package com.arrow2851.nudge.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arrow2851.nudge.core.mutation.AppFeedbackEvent
+import com.arrow2851.nudge.core.mutation.AppMutationCoordinator
+import com.arrow2851.nudge.core.mutation.UndoToken
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class AppShellViewModel @Inject constructor(
+    private val mutationCoordinator: AppMutationCoordinator,
+) : ViewModel() {
+    val feedback: SharedFlow<AppFeedbackEvent> = mutationCoordinator.events
+
+    fun undo(token: UndoToken) {
+        viewModelScope.launch { mutationCoordinator.performUndo(token) }
+    }
+
+    fun invalidateUndo() {
+        viewModelScope.launch { mutationCoordinator.invalidateUndo() }
+    }
+}
